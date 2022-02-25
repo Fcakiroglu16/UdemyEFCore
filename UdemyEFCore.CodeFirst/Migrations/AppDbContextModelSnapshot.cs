8e561bcfa6235774a01eeb014f5fd46b76fdec36
Fatih Çakıroğlu <fatih.cakiroglu.dn@aykargo.com.tr>
2022-02-25 17:41:22 +0300

    CodeFirst migration 2

--- UdemyEFCore.CodeFirst/Migrations/AppDbContextModelSnapshot.cs
+++ UdemyEFCore.CodeFirst/Migrations/AppDbContextModelSnapshot.cs
@@ -44,7 +44,7 @@ namespace UdemyEFCore.CodeFirst.Migrations
 
                     b.HasKey("Id");
 
-                    b.ToTable("Products");
+                    b.ToTable("Products", (string)null);
                 });
 #pragma warning restore 612, 618
         }

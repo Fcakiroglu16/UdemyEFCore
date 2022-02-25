861be5310a476604ebe975ffde7b899a341c2092
Fatih Çakıroğlu <fatih.cakiroglu.dn@aykargo.com.tr>
2022-02-25 21:32:42 +0300

    DbContext State

--- UdemyEFCore.CodeFirst/DAL/Product.cs
+++ UdemyEFCore.CodeFirst/DAL/Product.cs
@@ -10,7 +10,7 @@ namespace UdemyEFCore.CodeFirst.DAL
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Price { get; set; }
+        public decimal Price { get; set; }
         public int Stock { get; set; }
 
         public int Barcode { get; set; }

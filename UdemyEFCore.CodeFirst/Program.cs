94314e708c17d5e225da9361913f4d2069025905
Fatih Çakıroğlu <fatih.cakiroglu.dn@aykargo.com.tr>
2022-04-29 10:31:53 +0300

    EF Core | Transaction: Transaction Example

--- UdemyEFCore.CodeFirst/Program.cs
+++ UdemyEFCore.CodeFirst/Program.cs
@@ -18,66 +18,49 @@ Initializer.Build();
 using (var _context = new AppDbContext())
 {
 
-    //var productsDto = _context.Products.Select(x => new ProductDto()
-    //{
-    //    Id = x.Id,
-    //    Name = x.Name,
-    //    Price = x.Price,
-    //    DiscountPrice = x.DiscountPrice,
-    //    Stock = x.Stock
 
 
-    //}).ToList();
 
 
-    // var product = _context.Products.ToList();
+    using (var transaction = _context.Database.BeginTransaction())
+    {
 
-    //var productDto = ObjectMapper.Mapper.Map<List<ProductDto>>(product);
 
-    var productDto = _context.Products.ProjectTo<ProductDto>(ObjectMapper.Mapper.ConfigurationProvider).Where(x => x.Price > 10).ToList();
+        var category = new Category() { Name = "Kılıflar" };
+
+        _context.Categories.Add(category);
+        _context.SaveChanges();
+
+        Product product = new()
+        {
+            Name = "Kılıf 1",
+            Price = 100,
+            Stock = 200,
+            Barcode = 123,
+            DiscountPrice = 100,
+            CategoryId = category.Id
+        };
+
+        _context.Products.Add(product);
+
+        _context.SaveChanges();
+
+
+        transaction.Commit();
+
+
+
+    }
+
+
+
+
+
 
 
 
 
     Console.WriteLine("");
-
-
-
-
-
-
-
-
-
-
-
-    //var products = await _context.Products.Select(x => new ProductDto
-    //{
-    //    CategoryName = x.Category.Name,
-    //    ProductName = x.Name,
-    //    ProductPrice = x.Price,
-    //    Width = (int?)x.ProductFeature.Width
-
-
-    //}).Where(x => x.Width > 10).ToListAsync();
-
-
-
-    //var categories = _context.Categories.Select(x => new ProductDto2
-    //{
-    //    CategoryName = x.Name,
-    //    ProductNames = String.Join(",", x.Products.Select(z => z.Name)),
-    //    TotalPrice = x.Products.Sum(x => x.Price),
-    //    TotalWidth = (int?)x.Products.Select(x => x.ProductFeature.Width).Sum()
-
-    //}
-    //).Where(y => y.TotalPrice > 100).OrderBy(x => x.TotalPrice).ToList();
-
-
-
-
-
-
     #region Data Insert
     //var category = new Category() { Name = "Defterler" };
     //category.Products.Add(new() { Name = "Defter 1", Price = 100, Stock = 200, Barcode = 123, ProductFeature = new ProductFeature() { Color = "Red", Height = 200, Width = 100 } });
